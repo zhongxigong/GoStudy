85dcb6af080b6827eb28c65e50ba3af5cdbfa0a6
yaoyuan <yaoyaoyuan@weiyankeji.cn>
2019-02-10 10:16:19 +0800

    fix bug

---
+++ leetcode_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+*/
+func TestArraySum(t *testing.T) {
+	nums := []int{1, 5, 6, 2, 7, 11, 15}
+
+	target := 26
+
+	for index := 0; index < len(nums); index++ {
+		fmt.Println(index)
+		for innerIndex := index + 1; innerIndex < len(nums); innerIndex++ {
+			if nums[index]+nums[innerIndex] == target {
+				fmt.Println([]int{index, innerIndex})
+				break
+			}
+		}
+	}
+}
